8fdd5f824ce193bab1b49183518532dc52e27ea2
yozoon <yozoon.dev@gmail.com>
2020-12-05 13:16:30 +0100

    Added Google Play Store link to index page.

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,5 +1,10 @@
 <html>
-    <body>
-        <h1>eTrax|rescue App Download</h1>
-    </body>
+
+<body>
+    <h1>eTrax | rescue App Download</h1>
+    <p align="center">
+        <a href="https://play.google.com/store/apps/details?id=at.etrax.etrax_rescue_app&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"><img width="300" alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" /></a>
+    </p>
+</body>
+
 </html>
